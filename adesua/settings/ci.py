57cb0da9609c43479b666938296b43bab9b31b47
mickBoat00 <m.boateng0000@gmail.com>
2023-12-04 18:22:54 +0000

    add django settings for ci/cd

---
+++ adesua/settings/ci.py
@@ -0,0 +1,21 @@
+# ruff: noqa: F405
+# ruff: noqa: F403
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ["DEBUG"]
+
+
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
