9cedc087d64d13ffe07aa6d0a680b8004ac2b261
mickBoat00 <m.boateng0000@gmail.com>
2023-12-04 17:33:48 +0000

    change ci to test docker image

--- course/tests.py
+++ course/tests.py
@@ -1,1 +1,13 @@
-# Create your tests here.
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+client = APIClient()
+
+pytestmark = pytest.mark.django_db
+
+
+def test_curriculum_endpoint():
+    url = reverse("curriculum")
+    response = client.get(url)
+    assert response.status_code == 200
